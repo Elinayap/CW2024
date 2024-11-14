6b4bb48554bc2e433fb13e35fef5138d5c6ae634
Elinayap <elinajh0124@gmail.com>
2024-11-14 21:20:49 +0800

    Fix shield image

--- src/main/java/com/example/demo/LevelTwo.java
+++ src/main/java/com/example/demo/LevelTwo.java
@@ -1,20 +1,49 @@
 package com.example.demo;
 
+import javafx.util.Duration;
+
+import javafx.animation.PauseTransition;
+
 public class LevelTwo extends LevelParent {
 
 	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
 	private static final int PLAYER_INITIAL_HEALTH = 5;
 	private final Boss boss;
 	private LevelViewLevelTwo levelView;
+	private ShieldImage shieldImage;
+	public static final int SHIELD_SIZE = 200;
 
 	public LevelTwo(double screenHeight, double screenWidth) {
 		super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
-		boss = new Boss();
+
+		//Initialize the image at (0,0)
+    	shieldImage = new ShieldImage(0, 0);
+		boss = new Boss(shieldImage);
+		//Adjust the position of the image
+		double xOffset = 100;  
+    	double yOffset = 100; 
+
+    //Match the shield's X position with the boss plane's X position
+    shieldImage.layoutXProperty().bind(
+        boss.layoutXProperty()
+        .add(boss.translateXProperty())
+        .subtract(ShieldImage.SHIELD_SIZE / 2)
+        .add(xOffset)
+    );
+	///Match the shield's Y position with the boss plane's Y position
+    shieldImage.layoutYProperty().bind(
+        boss.layoutYProperty()
+        .add(boss.translateYProperty())
+        .subtract(ShieldImage.SHIELD_SIZE / 2)
+        .add(yOffset)
+	);
+		
 	}
 
 	@Override
 	protected void initializeFriendlyUnits() {
 		getRoot().getChildren().add(getUser());
+		getRoot().getChildren().add(shieldImage);
 	}
 
 	@Override
@@ -39,5 +68,20 @@ public class LevelTwo extends LevelParent {
 		levelView = new LevelViewLevelTwo(getRoot(), PLAYER_INITIAL_HEALTH);
 		return levelView;
 	}
+
+	@Override
+    public void startGame() {
+        super.startGame();
+	}
+		//To show the shield image
+		public void activateShield() {
+			shieldImage.showShield();
+			
+		}
+
+		//To hide the shield image
+		public void deactivateShield() {
+			shieldImage.hideShield();
+		}
 
 }
