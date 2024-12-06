6bb9c2b5128b99b7f6ccf248b5e8e03ec98c420e
Elinayap <elinajh0124@gmail.com>
2024-12-06 17:02:59 +0800

    Added ShopTest

--- src/test/java/com/example/demo/UI/ShopTest.java
+++ src/test/java/com/example/demo/UI/ShopTest.java
@@ -2,18 +2,75 @@ package com.example.demo.UI;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
+
+import com.example.demo.GameState.GameState;
 import com.example.demo.levels.LevelParent;
+
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import static org.mockito.Mockito.mock;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class ShopTest {
 
-// @Test
-// void testShopShow() {
-//     Stage stage = new Stage();
-//     LevelParent currentLevel = mock(LevelParent.class);
-//     Shop shop = new Shop(stage, currentLevel);
+    //Correctly display the shop
+ @Test
+    void testShopShow() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
 
-//     assertDoesNotThrow(() -> shop.show(), "Shop should display without errors.");
-// }
+        Platform.startup(() -> {
+            try {
+               
+                Stage stage = new Stage();
+                LevelParent currentLevel = mock(LevelParent.class);
+                Shop shop = new Shop(stage, currentLevel);
+
+                
+                assertDoesNotThrow(() -> shop.show());
+            } finally {
+                latch.countDown(); 
+            }
+        });
+        latch.await();
+    }
+
+    //Shop is locked
+    @Test
+    void testShopLocked() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Platform.startup(() -> {
+            try{
+                Stage stage = new Stage();
+                LevelParent currentLevel = mock(LevelParent.class);
+                Shop shop = new Shop(stage, currentLevel);
+
+                GameState.getInstance().setShopLocked(true);
+                assertDoesNotThrow(() -> shop.show());
+            }  finally{
+                latch.countDown(); 
+            }
+        });
+        latch.await(1, TimeUnit.SECONDS);
+    }
+
+    //Shop's popup message
+    @Test
+    void testShowShopPopup() throws InterruptedException {
+    CountDownLatch latch = new CountDownLatch(1);
+
+    Platform.startup(() -> {
+        try {
+            assertDoesNotThrow(() -> Shop.showShopPopup("", ""));
+        } finally {
+            latch.countDown(); 
+        }
+    });
+
+        latch.await(1, TimeUnit.SECONDS); 
+    }
+    
 
 }
